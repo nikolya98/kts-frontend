8c9f957709243af8ca286a303b575b45406f0b4d
nikolya98 <nikolya.knyazev.98@mail.ru>
2022-03-22 00:16:34 +0300

    some fixes

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import "@styles/index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
